c70ebf4679da7e5948e190504008d4c8702b7453
canary <mchemweno27@gmail.com>
2020-03-23 00:16:02 +0300

    make it better

--- whatsapp_bot/views.py
+++ whatsapp_bot/views.py
@@ -14,25 +14,14 @@ def WhatsappBot(request):
     answer = wikiWaka.getInfo(msg)
 
     resp = MessagingResponse()
-
-    start = -1600
-    stop = 0
-    step = 1600
-
-    for x in range(0, len(answer)):
-        start = start + 1600
-        stop = start + 1600
-
-        if (stop == step):
-            resp.message(answer[start:stop])
-
-        step = step + 1600
+    resp.message(answer)
 
     return HttpResponse(resp)
-
 
 @api_view(['POST'])
 def DeliveryStatus(request):
     print(request.data)
 
     return HttpResponse(status=200)
+
+
--- whatsapp_bot/wikiWaka.py
+++ whatsapp_bot/wikiWaka.py
@@ -5,11 +5,11 @@ import wikipedia
 def getInfo(message):
     try:
         answer = wikipedia.page(title=message, auto_suggest=True, redirect=True, preload=True)
-        # print(answer.content)
+        answerString = answer.content[:1600:]
+        print(answerString)
     except:
         return "please rephrase your question well"
 
-    return answer.content
-
+    return answerString
 
 

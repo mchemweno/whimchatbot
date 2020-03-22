67675daa901558932040504321a2e50d498a7039
canary <mchemweno27@gmail.com>
2020-03-22 20:23:33 +0300

    make it better

--- whatsapp_bot/wikiWaka.py
+++ whatsapp_bot/wikiWaka.py
@@ -1,10 +1,9 @@
 import wikipedia
-from html.parser import HTMLParser
 
 def GetInfo(message):
     try:
         answer = wikipedia.page(title=message, auto_suggest=True, redirect=True, preload=False)
-        print(type(answer.content))
+
     except:
         return "please rephrase your question well"
 

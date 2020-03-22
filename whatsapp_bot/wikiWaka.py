aeedaeb8d73cabbd8f424705864f84a04898d403
canary <mchemweno27@gmail.com>
2020-03-22 19:49:32 +0300

    make it better

--- whatsapp_bot/wikiWaka.py
+++ whatsapp_bot/wikiWaka.py
@@ -4,11 +4,11 @@ import wikipedia
 def GetInfo(message):
     try:
         answer = wikipedia.page(title=message, auto_suggest=True, redirect=True, preload=False)
-
-        print(answer.content)
+        np = ''
+        for i in answer.content:
+            np = np + i
+        print(np)
     except:
-        answer = "please rephrase your question well"
+        np = "please rephrase your question well"
 
-    return answer
-
-GetInfo("types of computers")
+    return np

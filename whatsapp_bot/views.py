893c9fd34e316d6f666c3d657dd5e1061ab31ada
canary <mchemweno27@gmail.com>
2020-03-22 17:20:57 +0300

    make it better

---
+++ whatsapp_bot/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse, HttpResponse
+from . import wikiWaka
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+@api_view(['POST'])
+def WhatsappBot(request):
+    print(request.data)
+    msg = request.data['Body']
+
+    answer = wikiWaka.GetInfo(msg)
+
+    resp = MessagingResponse()
+    resp.message(answer)
+
+    print(resp)
+    return HttpResponse(str(resp))
---
+++ whatsapp_bot/wikiWaka.py
@@ -0,0 +1,13 @@
+import wikipedia
+
+
+def GetInfo(message):
+    try:
+        answer = wikipedia.summary(message, sentences=5)
+        print(answer)
+    except:
+        answer = "please rephrase your question well"
+
+    return answer
+
+GetInfo("types of os")

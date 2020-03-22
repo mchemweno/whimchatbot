8656c64ce5d5b17801bda2c72f2ce1bdb4e35aec
canary <mchemweno27@gmail.com>
2020-03-22 18:25:34 +0300

    make it better

--- whatsapp_bot/wikiWaka.py
+++ whatsapp_bot/wikiWaka.py
@@ -3,9 +3,12 @@ import wikipedia
 
 def GetInfo(message):
     try:
-        answer = wikipedia.summary(message, sentences=5)
+        answer = wikipedia.summary(message, sentences=5000, auto_suggest=True)
+        # answer = wikipedia.page(title=None, pageid=100, auto_suggest=True, redirect=True, preload=False)
+
         print(answer)
     except:
         answer = "please rephrase your question well"
 
     return answer
+

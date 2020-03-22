4100c5bc704b435cb2318ce792cf7a225c103f7f
canary <mchemweno27@gmail.com>
2020-03-22 19:56:00 +0300

    make it better

--- whatsapp_bot/wikiWaka.py
+++ whatsapp_bot/wikiWaka.py
@@ -4,11 +4,8 @@ import wikipedia
 def GetInfo(message):
     try:
         answer = wikipedia.page(title=message, auto_suggest=True, redirect=True, preload=False)
-        np = ''
-        for i in answer.content:
-            np = np + i
-        print(np)
+        print(answer.content)
     except:
-        np = "please rephrase your question well"
+        answer = "please rephrase your question well"
 
-    return str(np)
+    return answer.content

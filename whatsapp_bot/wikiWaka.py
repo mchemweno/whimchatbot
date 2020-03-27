06b748295d769f347358947b6e2318fbd0090e02
canary <mchemweno27@gmail.com>
2020-03-27 05:30:34 +0300

    images

--- whatsapp_bot/wikiWaka.py
+++ whatsapp_bot/wikiWaka.py
@@ -8,7 +8,7 @@ def getInfo(message):
         answer = wikipedia.page(title=message, auto_suggest=True, redirect=True, preload=True)
         answerString = answer.content
     except:
-        return "please rephrase your question well"
+        answer = "please rephrase your question well"
 
     myImage = ''
     if answer.images[1]:
